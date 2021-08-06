d1541263ff31ac7cf74d652ac484d2710fc6eade
mao <mao@qq.com>
2021-08-06 09:24:28 +0800

    first commit

---
+++ src/main/java/demo/test.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * description：
+ *
+ * @author shkstart
+ * @create2021-08-06 9:21
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
